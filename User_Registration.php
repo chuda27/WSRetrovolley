bc5a4e0d1ad169388fed54a32d4176e11a53f2ab
Choirul Huda <choirulhuda07@gmail.com>
2021-02-23 22:40:06 +0700

    Delete unusable/command code

--- User_Registration.php
+++ User_Registration.php
@@ -41,66 +41,6 @@
         }
 
         print(json_encode($response));
-        
-        
-
-        // $user_email = $_POST['user_email']; 
-        // $user_password = $_POST['user_password'];
-        // $user_fullname = $_POST['user_fullname'];
-
-        // $query_check = "select * from user_detail where user_email = '$user_email'";
-
-        // $query_insert = "insert into user_detail (user_email, user_password, user_fullname) value 
-        // ('$user_email', '$user_password', '$user_fullname')";
-
-        // $json_array = array();
-        // $response = "";
-
-        // if (isset($query_check)) {
-        //     $response = array(
-        //         'code' => 406,
-        //         'status' => 'User has been registered!'
-        //     );
-        // } else {
-        //     if (mysqli_query($conn, $query_insert)) {
-        //         $response = array(
-        //             'code' => 200,
-        //             'status' => 'User Registered'
-        //         );
-        //     } else {
-        //         $response = array(
-        //             'code' => 405,
-        //             'status' => 'Registed Error!'
-        //         );
-        //     }
-        // }
-
-        // print(json_encode($response));
-
-        ////////////////////////////////////////////////////
-
-         //or die (mysqli_error($conn));
-
-        // $email = $_POST['user_email'];
-        // $pass = $_POST['user_password'];
-        // $fullname = $_POST['user_fullname'];
-
-        // $check_sql = "select * from user_detail where user_email = $email";
-        // $check = mysqli_fetch_array(mysqli_query($conn, $check_sql));
-
-        // if (isset($check)) {
-        //     echo 'Email is already exist, try another email!';
-        // } else {
-        //     $query_insert = "insert into user_detail (user_email, user_password, user_fullname 
-        //     values ($email, $pass, $fullname))";
-        //     if (mysqli_query($conn, $query_insert)) {
-        //         echo 'User is registered';
-        //     } else {
-        //         echo 'Something error, please check!';
-        //     }
-        // }
-
-
         mysqli_close($conn);
     } elseif($_SERVER['REQUEST_METHOD'] == 'GET') {
         include 'DatabaseConfig.php';
