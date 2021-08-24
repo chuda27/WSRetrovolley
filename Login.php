034bfa056fc007f492d5abc805a3238a483c0f80
Choirul Huda <choirulhuda07@gmail.com>
2021-08-24 13:08:18 +0700

    Initial Commit

---
+++ Login.php
@@ -0,0 +1,45 @@
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+        include 'DatabaseConfig.php';
+        $conn = mysqli_connect($HostName, $HostUser, $HostPass, $DatabaseName);
+
+        $email = $_GET['email'];
+        $password = $_GET['password'];
+
+        $query_check = "select * from user_detail where user_email = '$email'";
+        $check = mysqli_fetch_array(mysqli_query($conn, $query_check)); 
+        $json_array = array();
+        $response = "";
+
+        if (isset($check)) {
+            $query_check_pass = "select * from user_detail where user_email = '$email' and user_password = '$password'";
+            $query_pass_result = mysqli_query($conn, $query_check_pass);
+            $check_password = mysqli_fetch_array($query_pass_result);
+            if (isset($check_password)) {
+                $query_pass_result = mysqli_query($conn, $query_check_pass);
+                while ($row = mysqli_fetch_assoc($query_pass_result)) {
+                    $json_array[] = $row;
+                }                
+                $response = array(
+                    'code' => 200,
+                    'status' => 'Sukses',
+                    'user_list' => $json_array
+                );
+            } else {
+                $response = array(
+                    'code' => 401,
+                    'status' => 'Password salah, periksa kembali!',
+                    'user_list' => $json_array
+                );    
+            }
+        } else {
+            $response = array(
+                'code' => 404,
+                'status' => 'Data tidak ditemukan, lanjutkan registrasi?',
+                'user_list' => $json_array
+            );
+        }
+        print(json_encode($response));
+        mysqli_close($conn);
+    }
+?>
---
+++ User_Registration.php
@@ -0,0 +1,70 @@
+<?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        include 'DatabaseConfig.php';
+        $conn = mysqli_connect($HostName, $HostUser, $HostPass, $DatabaseName);        
+        
+        //read JSON from client
+        $json = file_get_contents('php://input', true);
+        $obj = json_decode($json);
+
+        //get JSON object
+        $fullname = $obj->user_fullname;
+        $email = $obj->user_email;
+        $password = $obj->user_password;
+
+        $query_check = "select * from user_detail where user_email = '$email'";
+        $check = mysqli_fetch_array(mysqli_query($conn, $query_check));        
+        $json_array = array();
+        $response = "";
+
+        if (isset($check)) {
+            $response = array(
+                'code' => 406,
+                'status' => 'User has been registered!'
+            );
+        } else {
+            $query_insert = "insert into user_detail (user_email, user_password, user_fullname) values 
+            ('$email', '$password', '$fullname')";
+            if (mysqli_query($conn, $query_insert)) {
+                $response = array(
+                    'code' => 201,
+                    'status' => 'User Registered'
+                );
+            } else {
+                $response = array(
+                    'code' => 405,
+                    'status' => 'Registered Error!'
+                );
+            }
+        }
+
+        print(json_encode($response));
+        mysqli_close($conn);
+    } elseif($_SERVER['REQUEST_METHOD'] == 'GET') {
+        include 'DatabaseConfig.php';
+        $conn = mysqli_connect($HostName, $HostUser, $HostPass, $DatabaseName);        
+        $query_insert = "select * from user_detail";
+        $result = mysqli_query($conn, $query_insert);
+        $json_array = array();
+        $response = "";
+
+        if (isset($result)) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $json_array[] = $row;
+            }
+            $response = array(
+                'code' => 200,
+                'status' => 'Successful',
+                'user_list' => $json_array
+            );   
+        } else {
+            $response = array(
+                'code' => 400,
+                'status' => 'Error',
+                'user_list' => 0
+            );
+        }
+        print(json_encode($response));
+        mysqli_close($conn);
+    }
+?>
